e8ca322f0cb89318b71b64f4d44846bcd1bd42fd
Sipxi <sipxi@email.cz>
2025-10-11 20:02:31 +0200

    added working test.c with debugger

--- prj/src/test.c
+++ prj/src/test.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include "lexer.h"
 
 /*
@@ -8,33 +7,47 @@
 Если хотите запустить этот файл:
 
 make test-pg
+
 */
 
+// Print token data safely, visualizing special characters
+void print_token_data(const char *data) {
+    for (const char *p = data; *p; p++) {
+        switch (*p) {
+            case '\n': printf("\\n"); break;
+            case '\t': printf("\\t"); break;
+            default:   putchar(*p); break;
+        }
+    }
+}
+
 int main() {
-    // printf("Hello, Lexer!\n");
+    // Use stdin for input (supports redirection like: ./test < input.wren)
     FILE *file = fopen("example.wren", "r");
+    //    FILE *file = stdin; --- IGNORE ---
     if (file == NULL) {
         fprintf(stderr, "Error opening file.\n");
         return 1;
     }
 
     Lexer *lexer = lexer_init();
-
-    if (lexer == NULL) {
+    if (!lexer) {
         fprintf(stderr, "Error initializing lexer.\n");
-        fclose(file);
         return 1;
     }
+
     while (lexer->current_token->type != TOKEN_EOF) {
         get_next_token(lexer, file);
-        printf("Token Type: %s, Data: %s, Line: %d\n",
-               token_type_to_string(lexer->current_token->type),
-               lexer->current_token->data,
-               lexer->current_token->line);
+
+        printf("Token Type: %s, Data: ",
+               token_type_to_string(lexer->current_token->type));
         
+        print_token_data(lexer->current_token->data);
+        
+        printf(", Line: %d\n", lexer->current_token->line);
     }
 
-    fclose(file);
+    // Don't close stdin
     lexer_free(lexer);
     return 0;
 }
