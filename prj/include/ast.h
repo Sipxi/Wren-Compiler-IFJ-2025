71889ef13b31f3a378356ecc6b8d1c5b111aee5b
Sipxi <sipxi@email.cz>
2025-11-12 12:42:55 +0100

    Исправлен стиль кода

--- prj/include/ast.h
+++ prj/include/ast.h
@@ -177,8 +177,8 @@ typedef struct AstNode {
     int line_number; // Номер строки для сообщений об ошибках
 
     //* --- 2. Структура Дерева (Заполняется в Pass 1) ---
-    struct AstNode* child;
-    struct AstNode* sibling;
+    struct AstNode *child;
+    struct AstNode *sibling;
 
     //* --- 3. Данные Узла (Заполняется в Pass 1) ---
     // Данные, специфичные для узла (в основном для листьев)
@@ -188,13 +188,13 @@ typedef struct AstNode {
          * NODE_ID, NODE_VAR_DEF, NODE_PARAM, NODE_FUNCTION_DEF,
          * NODE_SETTER_DEF, NODE_GETTER_DEF, NODE_TYPE_NAME
          */
-        char* identifier;
+        char *identifier;
 
         /** Используется для: NODE_LITERAL_NUM */
         double literal_num;
 
         /** Используется для: NODE_LITERAL_STRING */
-        char* literal_string;
+        char *literal_string;
 
     } data;
 
@@ -205,16 +205,16 @@ typedef struct AstNode {
      * (напр., узел NODE_OP_PLUS будет иметь data_type = TYPE_INT).
      * Для узлов-операторов (statements) это поле не используется.
      */
-    //==================================================================
-    //
-    // ПРИМЕР КАК ЭТО РАБОТАЕТ
-    //
-    // Pass 1 (Парсер)   : Устанавливает это в TYPE_UNKNOWN.
-    // Pass 2 (Семантика): *Вычисляет* и *заполняет* это поле
-    //                     (напр., узел 'a+b' получит TYPE_NUM).
-    // Pass 3 (Генератор): *Читает* это поле, чтобы генерировать
-    //                     правильный код (напр., ADD для Num).
-    //==================================================================
+     //==================================================================
+     //
+     // ПРИМЕР КАК ЭТО РАБОТАЕТ
+     //
+     // Pass 1 (Парсер)   : Устанавливает это в TYPE_UNKNOWN.
+     // Pass 2 (Семантика): *Вычисляет* и *заполняет* это поле
+     //                     (напр., узел 'a+b' получит TYPE_NUM).
+     // Pass 3 (Генератор): *Читает* это поле, чтобы генерировать
+     //                     правильный код (напр., ADD для Num).
+     //==================================================================
     DataType data_type;
 
     /**
@@ -226,42 +226,42 @@ typedef struct AstNode {
      * - NODE_FUNCTION_DEF (ссылка на новую запись в symtable)
      * ... и т.д.
      */
-    //==================================================================
-    //
-    // ПРИМЕР КАК ЭТО РАБОТАЕТ
-    //
-    // Pass 1 (Парсер)   : Устанавливает это в NULL.
-    //                     (Парсер знает только имя "a", но не
-    //                     что это "a" означает).
-    // Pass 2 (Семантика): Находит символ (напр., 'a') в symtable
-    //                     и заполняет это поле, устанавливая
-    //                     прямую ссылку на его TableEntry.
-    // Pass 3 (Генератор): Читает это поле, чтобы мгновенно
-    //                     получить данные о 'a' (напр., ее тип
-    //                     или где она хранится), без поиска по имени.
-    //==================================================================
-    TableEntry* table_entry;
+     //==================================================================
+     //
+     // ПРИМЕР КАК ЭТО РАБОТАЕТ
+     //
+     // Pass 1 (Парсер)   : Устанавливает это в NULL.
+     //                     (Парсер знает только имя "a", но не
+     //                     что это "a" означает).
+     // Pass 2 (Семантика): Находит символ (напр., 'a') в symtable
+     //                     и заполняет это поле, устанавливая
+     //                     прямую ссылку на его TableEntry.
+     // Pass 3 (Генератор): Читает это поле, чтобы мгновенно
+     //                     получить данные о 'a' (напр., ее тип
+     //                     или где она хранится), без поиска по имени.
+     //==================================================================
+    TableEntry *table_entry;
 } AstNode;
 
 
 /**
  * @brief Создает новый узел AST.
  * Данные (union) и семантические поля инициализируются в NULL/0.
- * @param type Тип узла (NodeTypgit coe).
+ * @param type Тип узла (NodeType).
  * @param line_number Номер строки для отладки.
  * @return Указатель на новый узел.
  */
-AstNode* ast_node_create(NodeType type, int line_number);
+AstNode *ast_node_create(NodeType type, int line_number);
 
 /**
  * @brief Рекурсивно освобождает узел и всех его детей и сиблингов.
  * !ВНИМАНИЕ: Эта функция также освобождает `data.identifier` и
  * !`data.literal_string`, если они не NULL.
- * 
+ *
  *
  * @param node Узел для освобождения (может быть NULL).
  */
-void ast_node_free_recursive(AstNode* node);
+void ast_node_free_recursive(AstNode *node);
 
 /**
  * @brief Добавляет 'new_child' в *конец* списка детей 'parent'.
@@ -273,27 +273,27 @@ void ast_node_free_recursive(AstNode* node);
  * @param parent Родительский узел (не должен быть NULL).
  * @param new_child Новый дочерний узел (не должен быть NULL).
  */
-void ast_node_add_child(AstNode* parent, AstNode* new_child);
+void ast_node_add_child(AstNode *parent, AstNode *new_child);
 
 
 /**
  * @brief (Для отладки) Печатает дерево, начиная с 'node'.
  * @param node Корневой узел для печати.
  */
-void ast_print_debug(AstNode* node);
+void ast_print_debug(AstNode *node);
 
 
 /* ======================================*/
 /* ===== 2. ПАРСЕР-ПОМОЩНИКИ (API) =====*/
 /* ======================================*/
 
-AstNode* ast_new_id_node(NodeType type, int line, const char* id);
+AstNode *ast_new_id_node(NodeType type, int line, const char *id);
 
-AstNode* ast_new_num_node(double value, int line);
+AstNode *ast_new_num_node(double value, int line);
 
-AstNode* ast_new_string_node(const char* value, int line);
+AstNode *ast_new_string_node(const char *value, int line);
 
-AstNode* ast_new_null_node(int line);
+AstNode *ast_new_null_node(int line);
 
-AstNode* ast_new_bin_op(NodeType type, int line, AstNode* left, AstNode* right);
+AstNode *ast_new_bin_op(NodeType type, int line, AstNode *left, AstNode *right);
 #endif
